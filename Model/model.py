74996e6f23219063591ccd2210eefd465084dfb6
tron <mercymcy5@gmail.com>
2018-05-22 10:52:01 +0300

    updated add operations on model

--- Model/model.py
+++ Model/model.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 
 filename = "scrapper_db"
 
@@ -33,20 +34,36 @@ def connect(filename):
         db.commit()
     return db
 
-def insert_scan():
+def insert_scan(url, last_modified=time.time()):
     cursor = db.connect()
+    cursor.execute("INSERT INTO url_scan "
+                   "(url, last_modified)"
+                   "VALUES (?, ?)", (url, last_modified))
+    db.commit()
 
-def insert_js_string():
-    pass
+def insert_js_string(string, regex, effect of js):
+    cursor = db.connect()
+    cursor.execute("INSERT INTO js_string "
+                   "(string, regex, effect_of_js)"
+                   "VALUES (?, ?, ?)", (string, regex, effect_of_js))
+    db.commit()
 
-def insert_scan_report():
-    pass
+def insert_scan_report(url, xss_result, scan_date):
+    cursor = db.connect()
+    cursor.execute("INSERT INTO scan_report "
+                   "(url, xss_result, scan_date)"
+                   "VALUES (?, ?, ?)", (url, xss_result, scan_date))
+    db.commit()
 
-def insert_positive_scan():
-    pass
+def insert_positive_scan(scan_id, url, string, other_js, effect_of_js, remedy):
+    cursor = db.connect()
+    cursor.execute("INSERT INTO positive_scan "
+                   "(scan_id, url, string, other_js, effect_of_js, remedy)"
+                   "VALUES (?, ?, ?, ?, ?, ?)", (scan_id, url, string, other_js, effect_of_js, remedy))
+    db.commit()
 
 def get_url():
-    pass
+    cursor = 
 
 def get_report():
     pass
