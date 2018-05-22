56fe85c103c03a59e3c61fd130f8bd587577c11f
tron <mercymcy5@gmail.com>
2018-05-22 15:48:50 +0300

    successfully did db connections

--- Model/model.py
+++ Model/model.py
@@ -1,69 +1,106 @@
 import sqlite3
-import time
+import time, os
 
 filename = "scrapper_db"
+db = None
+try:
+    
+    #create/connect to the database
+    def connect():
+        create = not os.path.exists(filename)
+        db = sqlite3.connect(filename)
+        if create:
+            cursor = db.cursor()
 
-#create/connect to the database
-def connect(filename):
-    create = not os.path.exists(filename)
-    db = sqlite3.connect(fileame)
-    if create:
+            cursor.execute("CREATE TABLE url_scan ("
+                           "scan_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
+                           "scan_date TEXT NOT NULL, "
+                           "url TEXT NOT NULL,"
+                           "last_modified DATETIME NOT NULL )")
+
+            cursor.execute("CREATE TABLE js_string ("
+                           "string_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,"
+                           "string TEXT NOT NULL,"
+                           "regex TEXT ,"
+                           "effect_of_js TEXT NOT NULL)")
+
+            cursor.execute("CREATE TABLE scan_report ("
+                           "report_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
+                           "url TEXT NOT NULL, "
+                           "XSS_result TEXT NOT NULL ,"
+                           "scan_date DATETIME NOT NULL)")
+
+            cursor.execute("CREATE TABLE positive_scan ("
+                           "positive_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
+                           "scan_id INTEGER NOT NULL,"
+                           "url TEXT NOT NULL,"
+                           "string TEXT NOT NULL,"
+                           "other_js_string TEXT NOT NULL,"
+                           "effect_of_js TEXT NOT NULL,"
+                           "remedy TEXT NOT NULL)")
+            db.commit()
+        return db
+
+    def insert_scan(url, scan_date, last_modified):
+        connect()
+        db = sqlite3.connect(filename)
         cursor = db.cursor()
-        cursor.execute("CREATE TABLE url_scan ("
-                       "scan_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
-                       "url TEXT NOT NULL,"
-                       "last_modified DATETIME NOT NULL )")
-        cursor.execute("CREATE TABLE js_string ("
-                       "string_id INTEGER PRIMARY KEY AUTOINCREMNET UNIQUE NOT NULL,"
-                       "string TEXT NOT NULL,"
-                       "regex TEXT ,"
-                       "effect_of_js TEXT NOT NULL)")
-        cursor.execute("CREATE TABLE scan_report ("
-                       "report_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,"
-                       "url TEXT NOT NULL ,"
-                       "XSS_result TEXT NOT NULL ,"
-                       "scan_date DATETIME NOT NULL)")
-        cursor.execute("CREATE TABLE positive_scan ("
-                       "positive_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,"
-                       "scan_id INTEGER NOT NULL,"
-                       "url TEXT NOT NULL,"
-                       "string TEXT NOT NULL,"
-                       "other_js_string TEXT NOT NULL,"
-                       "effect_of_js TEXT NOT NULL,"
-                       "remedy TEXT NOT NULL)")
+        cursor.execute("INSERT INTO url_scan "
+                       "(url, scan_date, last_modified)"
+                       "VALUES (?, ?, ?)", (url, scan_date, last_modified))
         db.commit()
-    return db
 
-def insert_scan(url, last_modified=time.time()):
-    cursor = db.connect()
-    cursor.execute("INSERT INTO url_scan "
-                   "(url, last_modified)"
-                   "VALUES (?, ?)", (url, last_modified))
-    db.commit()
+    def insert_js_string(string, regex, effect_of_js):
+        connect()
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO js_string "
+                       "(string, regex, effect_of_js)"
+                       "VALUES (?, ?, ?)", (string, regex, effect_of_js))
+        db.commit()
 
-def insert_js_string(string, regex, effect of js):
-    cursor = db.connect()
-    cursor.execute("INSERT INTO js_string "
-                   "(string, regex, effect_of_js)"
-                   "VALUES (?, ?, ?)", (string, regex, effect_of_js))
-    db.commit()
+    def insert_scan_report(url, xss_result, scan_date):
+        connect()
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO scan_report "
+                       "(url, xss_result, scan_date)"
+                       "VALUES (?, ?, ?)", (url, xss_result, scan_date))
+        db.commit()
 
-def insert_scan_report(url, xss_result, scan_date):
-    cursor = db.connect()
-    cursor.execute("INSERT INTO scan_report "
-                   "(url, xss_result, scan_date)"
-                   "VALUES (?, ?, ?)", (url, xss_result, scan_date))
-    db.commit()
+    def insert_positive_scan(scan_id, url, string, other_js, effect_of_js, remedy):
+        connect()
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO positive_scan "
+                       "(scan_id, url, string, other_js, effect_of_js, remedy)"
+                       "VALUES (?, ?, ?, ?, ?, ?)", (scan_id, url, string, other_js, effect_of_js, remedy))
+        db.commit()
 
-def insert_positive_scan(scan_id, url, string, other_js, effect_of_js, remedy):
-    cursor = db.connect()
-    cursor.execute("INSERT INTO positive_scan "
-                   "(scan_id, url, string, other_js, effect_of_js, remedy)"
-                   "VALUES (?, ?, ?, ?, ?, ?)", (scan_id, url, string, other_js, effect_of_js, remedy))
-    db.commit()
+    def get_url(url):
+        connect()
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        cursor.execute("SELECT scan_id FROM url_scan WHERE url = ?", (url,))
+        fields = cursor.fetchone()
+        return 1 if fields is not None else None
 
-def get_url():
-    cursor = 
+    def get_scan_date(url):
+        connect()
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        cursor.execute("SELECT scan_date FROM scan_report WHERE url = ?", (url))
+        fields = cursor.fetchone()
+        return fields[0] if fields is not None else None
 
-def get_report():
-    pass
+    def get_report(url):
+        connect()
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM scan_report WHERE url = ?", (url))
+        fields = cursor.fetchone()
+        return fields[0] if fields is not None else None
+
+finally:
+    if db is not None:
+        db.close()
