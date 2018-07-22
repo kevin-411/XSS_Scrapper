6695b39dafd0a20be89ad41f6d6d94bd7e1623c9
tron <mercymcy5@gmail.com>
2018-07-22 11:58:26 +0300

    replaced last_mdified with initial_hash, added 'order by' so as to get the latest record entries

--- Model/model.py
+++ Model/model.py
@@ -15,9 +15,10 @@ try:
 
             cursor.execute("CREATE TABLE url_scan ("
                            "scan_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
+                           "url TEXT NOT NULL, "
                            "scan_date TEXT NOT NULL, "
-                           "url TEXT NOT NULL,"
-                           "last_modified DATETIME NOT NULL )")
+                           "current_hash TEXT NOT NULL,"
+                           "initial_hash TEXT )")
 
             cursor.execute("CREATE TABLE js_string ("
                            "string_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,"
@@ -37,18 +38,19 @@ try:
                            "url TEXT NOT NULL,"
                            "string TEXT NOT NULL,"
                            "other_js_string TEXT NOT NULL,"
+                           "code_index TEXT NOT NULL,"
                            "effect_of_js TEXT NOT NULL,"
                            "remedy TEXT NOT NULL)")
             db.commit()
         return db
 
-    def insert_scan(url, scan_date, last_modified):
+    def insert_scan(url, scan_date, current_hash, initial_hash):
         connect()
         db = sqlite3.connect(filename)
         cursor = db.cursor()
         cursor.execute("INSERT INTO url_scan "
-                       "(url, scan_date, last_modified)"
-                       "VALUES (?, ?, ?)", (url, scan_date, last_modified))
+                       "(url, scan_date, current_hash, initial_hash)"
+                       "VALUES (?, ?, ?, ?)", (url, scan_date, current_hash, initial_hash))
         db.commit()
 
     def insert_js_string(string, regex, effect_of_js):
@@ -69,13 +71,13 @@ try:
                        "VALUES (?, ?, ?)", (url, xss_result, scan_date))
         db.commit()
 
-    def insert_positive_scan(scan_id, url, string, other_js, effect_of_js, remedy):
+    def insert_positive_scan(scan_id, url, string, other_js, current_code_index, effect_of_js, remedy):
         connect()
         db = sqlite3.connect(filename)
         cursor = db.cursor()
         cursor.execute("INSERT INTO positive_scan "
-                       "(scan_id, url, string, other_js_string, effect_of_js, remedy)"
-                       "VALUES (?, ?, ?, ?, ?, ?)", (int(scan_id), str(url), string, other_js, effect_of_js, remedy,))
+                       "(scan_id, url, string, other_js_string, code_index, effect_of_js, remedy)"
+                       "VALUES (?, ?, ?, ?, ?, ?, ?)", (int(scan_id), str(url), string, other_js, current_code_index, effect_of_js, remedy,))
         db.commit()
 
     def get_js_string(regex):
@@ -102,11 +104,11 @@ try:
         fields = cursor.fetchone()
         return 1 if fields is not None else None
 
-    def get_scan_date(url):
+    def get_initial_hash(url):
         connect()
         db = sqlite3.connect(filename)
         cursor = db.cursor()
-        cursor.execute("SELECT scan_date FROM url_scan WHERE url = ?", (url,))
+        cursor.execute("SELECT initial_hash FROM url_scan WHERE url = ? ORDER BY scan_id DESC", (url,))
         fields = cursor.fetchone()
         return fields[0] if fields is not None else None
 
@@ -114,7 +116,7 @@ try:
         connect()
         db = sqlite3.connect(filename)
         cursor = db.cursor()
-        cursor.execute("SELECT scan_id FROM url_scan WHERE url = ?", (url,))
+        cursor.execute("SELECT scan_id FROM url_scan WHERE url = ? ORDER BY scan_id DESC", (url,))
         fields = cursor.fetchone()
         return fields[0] if fields is not None else None
 
@@ -122,7 +124,7 @@ try:
         connect()
         db = sqlite3.connect(filename)
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM scan_report WHERE url = ?", (url,))
+        cursor.execute("SELECT * FROM scan_report WHERE url = ? ORDER BY report_id  DESC", (url,))
         fields = cursor.fetchone()
         return fields if fields is not None else None
     
@@ -130,7 +132,7 @@ try:
         connect()
         db = sqlite3.connect(filename)
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM positive_scan WHERE url = ?", (url, ))
+        cursor.execute("SELECT * FROM positive_scan WHERE url = ? ORDER BY positive_id DESC", (url, ))
         fields = cursor.fetchone()
         return fields if fields is not None else None
 
